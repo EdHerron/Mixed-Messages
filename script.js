9b139d4fb72fd6f042e5e1c8d5311bbd3837945c
Ed Herron <eherron325@gmail.com>
2023-03-11 16:10:12 +0000

    JS program created

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(10 + 5);
+const cookingMethod = ["boiled", "fried", "pulled", "baked", "BBQ'd", "roasted"];
+const proteinType = ["chicken", "beef", "pork", "duck", "turkey", "ham"];
+const servedWith = ["seasonal veg", "chips", "rice", "lentils", "a tortilla wrap", "nachoes", "potatoes"];
+const weeExtras = ["siracha", "tobacco onions", "mustard mayo", "pickled onions", "sundried toamtoes"];
+
+const getRandomElement = array => {
+    let arrayPosition = Math.floor(Math.random() * array.length);
+    let randomElement = array[arrayPosition];
+    return randomElement;
+};
+
+console.log("Tonight for dinner, you are having " + getRandomElement(cookingMethod) + " " + getRandomElement(proteinType) + " served with " + getRandomElement(servedWith) + ". If you'd like to jazz it up then try adding " + getRandomElement(weeExtras) + ". Enjoy!");
